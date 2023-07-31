ec17ea8853e741171dcec4dea5d8885f71b47dd7
gealtach <cleivaj93@gmail.com>
2023-07-31 19:43:48 -0400

    starting projects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='text-white'>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
