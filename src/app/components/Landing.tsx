675ea14b7311924ae3af19129f8bd5407b68f2ed
gealtach <cleivaj93@gmail.com>
2023-08-02 20:36:24 -0400

    scale

--- src/app/components/Landing.tsx
+++ src/app/components/Landing.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Landing = () => {
     return(
         <div className='flex flex-col mt-4 font-text h-screen' data-aos='fade-in' >
-            <div className="my-4 flex justify-center hover:scale-150">
-                <Image src={manDev} alt="dev" height={300} width={300} />
+            <div className="my-4 flex justify-center">
+                <Image className='hover:scale-150' src={manDev} alt="dev" height={300} width={300} />
             </div>
             <span className='text-3xl mt-2 md:mt-40'>Full-Stack <br /> Web and Mobile Developer.</span>
             <span className='my-4'>
