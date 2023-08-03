406c0287af8dd653fff07044c7689185851a0a2a
gealtach <cleivaj93@gmail.com>
2023-08-02 20:21:23 -0400

    about2021

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -14,60 +14,60 @@ import Image from 'next/image';
 const About = () => {
     return(
         <div className="font-text py-10 md:pb-20">
-            <div className='flex flex-col md:w-1/3 '>
+            <div className="flex flex-col md:w-1/3 ">
             <span className="text-3xl">Hi there I'm Cesar.</span>
             <span>
             I am a Full Stack Web developer with experience building web and mobile applications.
             I specialize in JavaScript and experience working with:
             </span>
             </div>
-            <div className='flex flex-auto justify-center flex-wrap m-2 p-2 rounded-xl'>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={jsLogo} alt='javaScript' className='w-10 m-2' />
+            <div className="flex flex-auto justify-center flex-wrap m-2 p-2 rounded-xl">
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={jsLogo} alt="JavaScript" className="w-10 m-2" />
                 <span>JavaScript</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={reactLogo} alt='React.js' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={reactLogo} alt="React.js" className="w-10 m-2" />
                     <span>React</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={reactLogo} alt='React-Native' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={reactLogo} alt="React-Native" className="w-10 m-2" />
                     <span>React-Native</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={reduxLogo} alt='Redux' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={reduxLogo} alt="Redux" className="w-10 m-2" />
                     <span>Redux</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={expressLogo} alt='Express.js' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={expressLogo} alt="Express.js" className="w-10 m-2" />
                     <span>Expres</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={nodeLogo} alt='Node.js' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={nodeLogo} alt="Node.js" className="w-10 m-2" />
                     <span>Node</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={nextLogo} alt='Next.js' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={nextLogo} alt="Next.js" className="w-10 m-2" />
                     <span>Next</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={gitLogo} alt='Git' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={gitLogo} alt="Git" className="w-10 m-2" />
                     <span>Git</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={postgreLogo} alt='PostgreSql' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={postgreLogo} alt="PostgreSql" className="w-10 m-2" />
                     <span>PostgreSql</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={prismaLogo} alt='Prisma' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={prismaLogo} alt="Prisma" className="w-10 m-2" />
                     <span>Prisma ORM</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={socketLogo} alt='socket.io' className='w-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={socketLogo} alt="Socket.io" className="w-10 m-2" />
                     <span>Socket.io</span>
                 </div>
-                <div className='flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125'>
-                    <Image src={tailwindLogo} alt='TailwindCss' className='w-10 h-10 m-2' />
+                <div className="flex flex-col items-center w-1/2 md:w-1/5 my-2 md:my-10 hover:scale-125">
+                    <Image src={tailwindLogo} alt="TailwindCss" className="w-10 m-2 h-10" />
                 <span>TailwindCss</span>
                 </div>
             </div>
